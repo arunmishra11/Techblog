6e2e8e834873ef172853d7e7ca6f321031ec3916
arunmishra11 <156019606+arunmishra11@users.noreply.github.com>
2024-06-23 13:02:19 -0500

    setting up database connection,sequelize

---
+++ config/connection.js
@@ -0,0 +1,20 @@
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+console.log("Database name:", process.env.DB_NAME);
+console.log("Database user:", process.env.DB_USER);
+console.log("Database password:", process.env.DB_PASSWORD ? "Loaded" : "Not loaded");
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'postgres',
+    port: process.env.DB_PORT || 5432
+  });
+}
+
+module.exports = sequelize;
